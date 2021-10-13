fa8e4f0e6069693d181cc161f257e9d177b12d4e
Salles <senirsales@hotmail.com>
2021-10-12 23:48:19 -0300

    Model Alunos

---
+++ ResultadosAlunosMVC/Models/Alunos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResultadosAlunosMVC.Models
+{
+    public class Alunos
+    {
+        public int Id { get; set; }
+
+        [StringLength(80, MinimumLength = 3)]
+        [Required]
+        [Display(Name = "Aluno")]
+        public string Nome { get; set; }
+        public int Id_Notas { get; set; }
+        public int Id_Faltas { get; set; }
+    }
+}
---
+++ ResultadosAlunosMVC/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResultadosAlunosMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Alunos> Alunos { get; set; }
+        //public DbSet<Alunos> Notas { get; set; }
+        //public DbSet<Alunos> Faltas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Resultadosalunos;Integrated Security=true");
+        }
+    }
+}
